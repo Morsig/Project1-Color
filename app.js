91c33c6ba36b117d8cb3fbd072eaa3df363c8aa0
Серёжа <ne370500@gmail.com>
2022-11-16 20:05:48 +0300

    hash

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const cols = document.querySelectorAll('.col')
 
-document.addEventListener('keydown', (event)=> {
+document.addEventListener('keydown', (event) => {
     event.preventDefault()
     if (event.code.toLowerCase() === 'space') {
         setRandomColors()
     }
-} )
+})
 
-document.addEventListener('click', (event)=>{
+document.addEventListener('click', (event) => {
     const type = event.target.dataset.type
 
     if (type === 'lock') {
-        const node = 
-        event.target.tagName.toLowerCase() === 'i'
-            ? event.target
-            : event.target.children[0]
+        const node =
+            event.target.tagName.toLowerCase() === 'i'
+                ? event.target
+                : event.target.children[0]
 
         node.classList.toggle('fa-lock-open')
         node.classList.toggle('fa-lock')
@@ -29,21 +29,27 @@ function gerenerateRandomColor() {
     for (let i = 0; i < 6; i++) {
         color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
     }
-    return '#'+color
+    return '#' + color
 }
 function copyToClickboard(text) {
-     return navigator.clipboard.writeText(text)
+    return navigator.clipboard.writeText(text)
 }
 
 function setRandomColors() {
+    const colors = []
+
     cols.forEach((col) => {
         const isLocked = col.querySelector('i').classList.contains('fa-lock')
         const text = col.querySelector('h2')
         const button = col.querySelector('button')
         const color = gerenerateRandomColor()
-        if(isLocked){
+
+        if (isLocked) {
+            colors.push(text.textContent)
             return
         }
+
+        colors.push(color)
 
         text.textContent = color
         col.style.background = color
@@ -51,11 +57,31 @@ function setRandomColors() {
         setTextColor(text, color)
         setTextColor(button, color)
     })
+
+    updateColorsHash(colors)
 }
 
 function setTextColor(text, color) {
     const luminance = chroma(color).luminance()
     text.style.color = luminance > 0.5 ? 'black' : 'white'
 }
+
+function updateColorsHash(colors = []) {
+    document.location.hash = colors.map((col) => col.toString().substring(1)).join('-')
+
+
+}
+
+function getColorsFromHash() {
+    if (document.location.hash.length > 1) {
+        return document.location.hash
+        .substring(1)
+        .split('-')
+        .map(color => '#' + color)
+
+    }
+    return []
+}
+
 
 setRandomColors()
